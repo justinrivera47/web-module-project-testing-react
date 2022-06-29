e6f1db8b4fb9cce60f24eae838782ad2b0f09131
justinrivera47 <justinrivera47@gmail.com>
2022-06-29 16:00:16 -0400

    got some testing done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,38 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+  id: 1,
+  name: "",
+  image: "http://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg",
+  season: 1,
+  number: 1,
+  summary: "test summary",
+  runtime: 1
+}
 
-test("renders the summary test passed as prop", () => { });
+const testEpisodeImage = {
+  id: 1,
+  name: "",
+  image: null,
+  season: 1,
+  number: 1,
+  summary: "test summary",
+  runtime: 1
+}
 
-test("renders default image when image is not defined", () => { });
+test("renders without error", () => { 
+  render(<Episode episode={testEpisode}/>)
+});
+
+test("renders the summary test passed as prop", () => { 
+  render(<Episode episode={testEpisode}/>)
+  const summary = screen.queryByText(/test summary/i);
+  expect(summary).toBeInTheDocument()
+  expect(summary).toBeTruthy()
+  expect(summary).toHaveTextContent('test summary')
+});
+
+test("renders default image when image is not defined", () => { 
+  render(<Episode episode={testEpisodeImage}/>)
+});
